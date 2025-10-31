9aef6979dd3314cad718fd84fe47a13acce2aa45
Usman Farooq <usmanfarooq317@gmail.com>
2025-10-31 15:06:06 +0500

    Add test file for Jenkins

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
-# app.py
-print("Hello from Jenkins Python build!")
+# test_app.py
 
+def test_message():
+    message = "Hello from Jenkins Python build!"
+    assert message == "Hello from Jenkins Python build!"
+
+if __name__ == "__main__":
+    test_message()
+    print("✅ Test passed successfully!")
